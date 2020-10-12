6120bee6e20d08a3f91b02f8d47f8b61ed235d96
dyoder838 <70074229+dyoder838@users.noreply.github.com>
2020-10-12 15:53:42 -0700

    fixed id bug in post section of apiRoutes.js, added comments

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # Note-Taker
+https://dans-note-taker.herokuapp.com/ | https://git.heroku.com/dans-note-taker.git
--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,70 +1,84 @@
-// link the api routes to the database, so that, user input can be stored and displayed
-
+// Link the api routes to the database, so that, user input can be stored and displayed.
+    // This is for writing notes to the database. 
 const fs = require("fs");
+    // This is to support database writing. 
 const path = require("path")
+    // This references the database - here we are defining where the database is and how to read it. 
 let noteData = fs.readFileSync(path.join(__dirname, "../db/db.json"), "utf8");
-noteData = JSON.parse(noteData)
+    // The database is JSON, so it needs to be translated - because of this, stringify will need to be used later. 
+noteData = JSON.parse(noteData);
 
-
+// Export the apiRoutes so they can be accessed by ajax
 module.exports = function(app) {
-//Get requests
-    // these show the user what is already in db.json, and updates as push requests are added 
 
-    // Get all of the data from the database
+//Get requests
+    // These show the user what is already in db.json, and updates as push requests are added. 
+
+    // Get all of the data from the database.
     app.get("/api/notes", function(req, res) {
-        // Send a response to the server
+        
+        // Send a response to the server.
         res.json(noteData);
     });
 
-    // Get specific data by id, so that, you can select an individual note to display, and delete a note later
+    // Get specific data by id, so that, you can select an individual note to display, and delete a note later.
     app.get("/api/notes/:id", function(req, res) {
-        // Send a response to the server
+       
+        // Send a response to the server.
         res.json(data[Number(req.params.id)]);
     });
 
-// Post request
+// Post request.
     
-    // These add the users input 
+    // These add the users input. 
     app.post("/api/notes", function(req, res) {
-        // req.body is the users input, we are calling it newNote, we need to give it a unique id
+        // req.body is the users input, we are calling it newNote, we need to give it a unique id.
         const newNote = req.body;
-        let noteId = newNote.title + (Math.floor(Math.random() * 10));
+        // there are many ways to generate a unique id, this one uses a time stamp as an id.
+        let noteId = Date.now();
         
-        // add the id to the newNote
+        // add the id to the newNote.
         newNote.id = noteId;
     
-        // Add the newNote to the database
+        // Add the newNote to the database.
         noteData.push(newNote);
     
-        // Update the database
+        // Update the database.
         fs.writeFile(path.join(__dirname, "../db/db.json"), JSON.stringify(noteData), function(err) {
-        // Send a response to the server
+        // Send a response to the server.
         res.json(newNote);
         });
     
     });
 
-// Delete requests
+// Delete requests.
 
-    // Delete notes by their id
+    // Delete notes by their id.
     app.delete("/api/notes/:id", function(req, res) {
-        // Select the note by its id  - this comes from the delete button on the front end
+        
+        // Select the note by its id  - this comes from the delete button on the front end.
         const deleteNote = req.params.id;
-        // Make an empty array to hold changes
+
+        // Make an empty array to hold changes.
         let savedNotes = [];
-        // Loop over all saved notes from the database
+        
+        // Loop over all saved notes from the database.
         for(let i = 0; i < noteData.length; i++){
-            // Find the keepers, exclude the note to be deleted - so indirectly deleting the note 
+
+            // Find the keepers, exclude the note to be deleted - indirectly deleting the note - there are multiple ways to do this.
             if (noteData[i].id !== deleteNote){
                 savedNotes.push(noteData[i])
             }
         }
-        // Update the screen
+        
+        // Add the notes back to the database.
         noteData = [...savedNotes] 
-        // Update the database
+
+        // Update the database.
         fs.writeFile(path.join(__dirname, "../db/db.json"), JSON.stringify(savedNotes), function(err) {
         });
-        // Send a response to the server
+
+        // Send a response to the server.
         res.send("DELETE request called");
     });
 
